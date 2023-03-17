92518d1139ca34b3ea6ff9aa63f76b07de2ba8ea
Matias Rodriguez <rodriguez.matias.g@gmail.com>
2023-03-17 20:00:22 -0300

    creando projects modal

--- my-Porfolio/my-portfolio/src/components/main/Main.jsx
+++ my-Porfolio/my-portfolio/src/components/main/Main.jsx
@@ -42,32 +42,30 @@ export default function Main() {
 
     return (
         <section className="banner" id="main">
-            <div className="blur-bg">
-                <Container >
-                    <Row className="align-items-center">
-                        <Col xs={12} md={7} xl={8}>
-                            <span className="tagline">Welcome to my Portfolio
-                            </span>
-                            <div className="text-h1-title">
-                                <h1>{`Hi I'm `}<br></br><span className="wrap"> {text}</span></h1>
-                            </div>
-                            <p> I'm Matías. I have a Full Stack Developer profile with background in industrial design. Currently, I am a teacher in technical high schools, but I also have work experience in the productive industry and sales...</p>
-                            <button className="banner-button" onClick={() => console.log('download')}>Resume <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
-                                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"></path>
-                                <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"></path>
-                            </svg></button>
-                        </Col>
-                        <Col xs={12} md={5} xl={4}>
-                            <div className="graphics">
-                                <a href="#about" className="grph1" style={{ color: 'var(--blackMed)' }}><div ><h2>Me</h2></div></a>
-                                <a href="#projects" className="grph2" style={{ color: 'var(--whiteDirty)' }}><div ><h2>Pr</h2></div></a>
-                                <a href="#works" className="grph3" style={{ color: 'var(--whiteDirty)' }}><div ><h2>Wk</h2></div></a>
-                                <a href="#formation" className="grph4" style={{ color: 'var(--blackMed)' }}><div ><h2>Fm</h2></div></a>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
+            <Container >
+                <Row className="align-items-center">
+                    <Col xs={12} md={7} xl={8}>
+                        <span className="tagline">Welcome to my Portfolio
+                        </span>
+                        <div className="text-h1-title">
+                            <h1>{`Hi I'm `}<br></br><span className="wrap"> {text}</span></h1>
+                        </div>
+                        <p> I'm Matías. I have a Full Stack Developer profile with background in industrial design. Currently, I am a teacher in technical high schools, but I also have work experience in the productive industry and sales...</p>
+                        <button className="banner-button" onClick={() => console.log('download')}>Resume <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+                            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"></path>
+                            <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"></path>
+                        </svg></button>
+                    </Col>
+                    <Col xs={12} md={5} xl={4}>
+                        <div className="graphics">
+                            <a href="#about" className="grph1" style={{ color: 'var(--blackMed)' }}><div ><h2>Me</h2></div></a>
+                            <a href="#projects" className="grph2" style={{ color: 'var(--whiteDirty)' }}><div ><h2>Pr</h2></div></a>
+                            <a href="#works" className="grph3" style={{ color: 'var(--whiteDirty)' }}><div ><h2>Wk</h2></div></a>
+                            <a href="#formation" className="grph4" style={{ color: 'var(--blackMed)' }}><div ><h2>Fm</h2></div></a>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
 
         </section>
     )
---
+++ my-Porfolio/my-portfolio/src/components/projects/dataProjetcs.js
@@ -0,0 +1,22 @@
+const dataProjects = [
+    {
+        id: 1,
+        name: "StreetArt Wine Club",
+        descrption: "development of a wine club, with e-commerce",
+        whatIdo: "I was in charge of the visual development, its interface, user behavior, the home page, and the administrator's dashboard",
+        images: ['../../../assets/streetArt/page1.jpg', '../../../assets/streetArt/', '../../../assets/streetArt/page1.jpg'],
+        tecnologies: ['React', 'JavaScript', 'Bootstrap', 'Material UI', 'nodeJs', 'express', 'Postgres', 'Sequelize', 'Redux'],
+        date: "February 2023",
+        link: "https://streetart-wine-club.vercel.app/"
+    },
+    {
+        id: 2,
+        name: "Alura",
+        descrption: "practice course, where you work on front responsive development, the behavior of pure DOM components",
+        whatIdo: "Practical work of the course, I was in charge of the development of components, their behavior and rendering of properties.",
+        images: ['../../../assets/alura/apeperia.png', '../../../assets/alura/encriptador.jpg', '../../../assets/alura/frutosyfrutas.png'],
+        tecnologies: ['HTML', 'JavaScript', 'CSS'],
+        date: "March 2023",
+        link: "https://github.com/Matias-Rodriguez-UX/Alura"
+    },
+]
