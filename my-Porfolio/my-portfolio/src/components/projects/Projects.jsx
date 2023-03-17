31a67dba531f79191875d47db57c2199492607f7
Matias Rodriguez <rodriguez.matias.g@gmail.com>
2023-03-17 14:26:08 -0300

    agregado imagenes

--- my-Porfolio/my-portfolio/src/components/projects/Projects.jsx
+++ my-Porfolio/my-portfolio/src/components/projects/Projects.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import './Projects.css'
 
 export default function Projects() {
-    const street = ''
 
     return (
         <section className="projects" id="projects">
             <div className="container mt-4">
                 <h1>Projects</h1>
-                <div className="d-flex">
-
+                <div className="images-projects">
+                    <div className="project-img1"></div>
+                    <div className="project-img2"></div>
+                    <div className="project-img3"></div>
+                    <div className="project-img4"></div>
+                    <div className="project-img5"></div>
+                    <div className="project-img6"></div>
+                    <div className="project-img7"></div>
+                    <div className="project-img8"></div>
                 </div>
             </div>
         </section>
